bdb5d3e5ab817c5e45cc525db4dfbbedf50a7dfe
rizwanullah313 <ru323091@gmail.com>
2022-01-30 21:15:43 +0500

    work on different file extension with css and link different pages

--- index.js
+++ index.js
@@ -91,6 +91,56 @@ const server = http.createServer((req, res)=>{
     let filePath = path.join(__dirname, 'public', req.url==='/'? 'index.html' : req.url);
     // console.log(filePath);
     // res.end();  
+    
+    // Extension of File
+    let extname = path.extname(filePath);
+
+    // Initial Content Type
+    let contentType = 'text/html';
+
+    //check ext and set content type
+    switch(extname){
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if(err){
+            if(err.code == 'ENOENT'){
+                // page not found
+
+                fs.readFile(path.join(__dirname, 'public', '404.html'),(err, content)=>
+                {
+                        res.writeHead(200,{'Content-Type': 'text/html'});
+                        res.end(content, 'utf8');
+                })
+            }
+            else{
+                // some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        }
+        else{
+            // success
+            res.writeHead(200,{'Content-Type': contentType});
+            res.end(content, 'utf8');
+        }
+    });
     });
 
 const PORT = process.env.PORT || 5000;
