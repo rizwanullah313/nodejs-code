cb8db67aa75e8b1428abbc1b5a3c03bea7515438
rizwanullah313 <ru323091@gmail.com>
2022-01-30 18:58:25 +0500

    Create and write file

--- references/fs_demo.js
+++ references/fs_demo.js
@@ -8,3 +8,8 @@ fs.mkdir(path.join(__dirname, '/test'), {}, err => {
     console.log('Folder Created...');
 });
 
+// Create and Write to File
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),'Hello World I am rizwan Ullah', err => {
+    if(err) throw err;
+    console.log('File Written to...');
+});
