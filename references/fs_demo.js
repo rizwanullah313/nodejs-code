9035a55c0e10feb63e24bcbc0c956072c4329925
rizwanullah313 <ru323091@gmail.com>
2022-01-30 19:08:56 +0500

    create , write and read file

--- references/fs_demo.js
+++ references/fs_demo.js
@@ -3,13 +3,30 @@ const path = require('path');
 
 
 // Create Folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-    if(err) throw err;
-    console.log('Folder Created...');
-});
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if(err) throw err;
+//     console.log('Folder Created...');
+// });
 
 // Create and Write to File
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),'Hello World I am rizwan Ullah', err => {
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),'Hello World I am rizwan Ullah', err => {
+//     if(err) throw err;
+//     console.log('File Written to...');
+// });
+
+// if uper wala copy kerky same to same run kerny our details change kerny per text overwrite hojyega
+// ager two time use kerna howa tu appendfile kerna hoga
+
+// Create and Write to File
+
+
+// fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),'I am a PROGRAMMER!', err => {
+//     if(err) throw err;
+//     console.log('File Written to...');
+// });
+
+// Read File    
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'),'utf8', (err, data) => {
     if(err) throw err;
-    console.log('File Written to...');
+    console.log(data);
 });
